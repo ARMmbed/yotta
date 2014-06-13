ab754ca9304e5ad7781f105d47a8d37900508dce
James Crosby <James.Crosby@arm.com>
2014-06-13 12:04:53 +0100

    use manifest file to ensure readme is included in distribution

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name = "yotta",
-    version = "0.0.13",
+    version = "0.0.14",
     author = "James Crosby",
     author_email = "James.Crosby@arm.com",
     description = ("Re-usable components for embedded software."),
