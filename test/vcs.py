dbfbea2d111c4fdc08499107de6c1db4211508e2
Bogdan Marinescu <bogdan.marinescu@arm.com>
2014-07-09 17:22:19 +0100

    Added basic tests for mercurial

--- test/vcs.py
+++ test/vcs.py
@@ -4,6 +4,7 @@
 import unittest
 import os
 import subprocess
+import hgapi
 
 # vcs, , represent version controlled directories, internal
 from yotta.lib import vcs
@@ -11,7 +12,8 @@ from yotta.lib import vcs
 from yotta.lib import fsutils
 
 
-Test_Repo = "git@github.com:autopulated/testing-dummy.git"
+Test_Repo_git = "git@github.com:autopulated/testing-dummy.git"
+Test_Repo_hg  = "ssh://hg@bitbucket.org/autopulated/hg-testing-dummy"
 
 class TestGit(unittest.TestCase):
     def setUp(self):
@@ -29,7 +31,7 @@ class TestGit(unittest.TestCase):
             for cmd in commands:
                 child = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             out, err = child.communicate()
-        self.working_copy = vcs.Git.cloneToTemporaryDir(Test_Repo)
+        self.working_copy = vcs.Git.cloneToTemporaryDir(Test_Repo_git)
         self.assertTrue(self.working_copy)
         
     def tearDown(self):
@@ -40,6 +42,33 @@ class TestGit(unittest.TestCase):
         fsutils.rmF(os.path.join(self.working_copy.workingDirectory(), 'package.json'))
         self.assertFalse(self.working_copy.isClean())
     
+    def test_commit(self):
+        with open(os.path.join(self.working_copy.workingDirectory(), 'package.json'), "a") as f:
+            f.write("\n")
+        self.working_copy.markForCommit('package.json')
+        self.working_copy.commit('test commit: DO NOT PUSH')
+        self.assertTrue(self.working_copy.isClean())
+
+# FIXME: refactor common code in a single base class
+class TestHg(unittest.TestCase):
+    def setUp(self):
+        # test if we have a git user set up, if not we need to set one
+        info = hgapi.Repo.command(".", os.environ, "showconfig")
+        if info.find("ui.username") == -1:
+            # hg doesn't provide a way to set the username from the command line.
+            # The HGUSER environment variable can be used for that purpose.
+            os.environ['HGUSER'] = 'Yotta Test <test@yottabuild.org>'
+        self.working_copy = vcs.HG.cloneToTemporaryDir(Test_Repo_hg)
+        self.assertTrue(self.working_copy)
+
+    def tearDown(self):
+        self.working_copy.remove()
+
+    def test_isClean(self):
+        self.assertTrue(self.working_copy.isClean())
+        fsutils.rmF(os.path.join(self.working_copy.workingDirectory(), 'package.json'))
+        self.assertFalse(self.working_copy.isClean())
+
     def test_commit(self):
         with open(os.path.join(self.working_copy.workingDirectory(), 'package.json'), "a") as f:
             f.write("\n")
