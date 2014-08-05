9e9b1ab276f74117a833eb3cdbc1bde2e1d48f3e
Boyu Fang <Boyu.Fang@arm.com>
2014-08-05 14:28:41 +0100

    Formatting code to PEP8

--- yotta/init.py
+++ yotta/init.py
@@ -8,11 +8,12 @@ from lib import component
 from lib import version
 
 Known_Licenses = {
-    'mit' : 'https://spdx.org/licenses/MIT',
-    'bsd-3-clause' : 'https://spdx.org/licenses/BSD-3-Clause'
+    'mit': 'https://spdx.org/licenses/MIT',
+    'bsd-3-clause': 'https://spdx.org/licenses/BSD-3-Clause'
 }
 
-def getUserInput(question, default = None, type_class=str):
+
+def getUserInput(question, default=None, type_class=str):
     while True:
         default_descr = ''
         if default is not None:
@@ -27,6 +28,7 @@ def getUserInput(question, default = None, type_class=str):
             print '"%s" isn\'t a valid "%s" value' % (value, type_class.__name__)
     return typed_value
 
+
 def addOptions(parser):
     pass
 
@@ -35,28 +37,33 @@ def execCommand(args):
     cwd = os.getcwd()
     c = component.Component(cwd)
     if c:
-        logging.info('The current directory already a contains a component: existing description will be modified')
+        logging.info(
+            'The current directory already a contains a component: existing description will be modified')
     elif os.path.isfile(c.getDescriptionFile()):
-        logging.error('A component description exists but could not be loaded:')
+        logging.error(
+            'A component description exists but could not be loaded:')
         logging.error(c.error)
         return 1
-    
+
     c.setName(getUserInput("Enter the package name:", c.getName()))
-    c.setVersion(getUserInput("Enter the initial version:", str(c.getVersion() or "0.0.0"), version.Version))
+    c.setVersion(
+        getUserInput("Enter the initial version:", str(c.getVersion() or "0.0.0"), version.Version))
 
     def current(x):
         return c.description[x] if x in c.description else None
 
-    c.description['description'] = getUserInput("Short description: ", current('description'))
+    c.description['description'] = getUserInput(
+        "Short description: ", current('description'))
     c.description['author'] = getUserInput("Author: ", current('author'))
     c.description['homepage'] = getUserInput("Homepage: ", current('homepage'))
 
     if not current('licenses') or current('license'):
-        license = getUserInput('What is the license for this project (MIT, BSD-3-Clause, etc.)? ')
+        license = getUserInput(
+            'What is the license for this project (MIT, BSD-3-Clause, etc.)? ')
         license_url = 'about:blank'
         if license.lower().strip() in Known_Licenses:
             license_url = Known_Licenses[license.lower().strip()]
-        c.description['licenses'] = [{'type':license, 'url':license_url}]
+        c.description['licenses'] = [{'type': license, 'url': license_url}]
 
     c.description['dependencies'] = current('dependencies') or {}
     c.description['targetDependencies'] = current('targetDependencies') or {}
@@ -65,4 +72,3 @@ def execCommand(args):
     # the registry...
 
     c.writeDescription()
-
