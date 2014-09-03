9357d163e13a0e7bff481e6453a0dec7ebd95a72
James Crosby <James.Crosby@arm.com>
2014-09-03 15:40:19 +0100

    bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name = "yotta",
-    version = "0.0.23",
+    version = "0.0.24",
     author = "James Crosby",
     author_email = "James.Crosby@arm.com",
     description = ("Re-usable components for embedded software."),
