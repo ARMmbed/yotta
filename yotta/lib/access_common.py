77a3464e7bc9b2cee52bda498c07c0223ea25a6c
James Crosby <James.Crosby@arm.com>
2014-11-05 16:38:33 +0000

    unpack tarballs into adjacent temporary directory, then move into place (fixes #50)

--- yotta/lib/access_common.py
+++ yotta/lib/access_common.py
@@ -8,6 +8,8 @@ import tarfile
 import logging
 import os
 import hashlib
+import tempfile
+import shutil
 
 # version, , represent versions and specifications, internal
 import version
@@ -56,40 +58,56 @@ def unpackTarballStream(stream, into_directory, hash=(None, None)):
     if hash_name:
         m = getattr(hashlib, hash_name)()
     chunk = 1024 * 32
-    fsutils.mkDirP(into_directory)
-    download_fname = os.path.join(into_directory, 'download.tar.gz')
+    into_parent_dir = os.path.dirname(into_directory)
+    fsutils.mkDirP(into_parent_dir)
+    temp_directory = tempfile.mkdtemp(dir=into_parent_dir)
+    download_fname = os.path.join(temp_directory, 'download.tar.gz')
     # remove any partially downloaded file: TODO: checksumming & caching of
     # downloaded components in some central place
     fsutils.rmF(download_fname)
     # create the archive exclusively, we don't want someone else maliciously
     # overwriting our tar archive with something that unpacks to an absolute
     # path when we might be running sudo'd
-    fd = os.open(download_fname, os.O_CREAT | os.O_EXCL | os.O_RDWR)
-    with os.fdopen(fd, 'rb+') as f:
-        f.seek(0)
-        while True:
-            data = stream.read(chunk)
-            if not data: break
+    try:
+        fd = os.open(download_fname, os.O_CREAT | os.O_EXCL | os.O_RDWR)
+        with os.fdopen(fd, 'rb+') as f:
+            f.seek(0)
+            while True:
+                data = stream.read(chunk)
+                if not data: break
+                if hash_name:
+                    m.update(data)
+                f.write(data)
             if hash_name:
-                m.update(data)
-            f.write(data)
-        if hash_name:
-            calculated_hash = m.hexdigest()
-            logging.debug('calculated hash: %s check against: %s' % (calculated_hash, hash_value))
-            if hash_value and (hash_value != calculated_hash):
-                raise Exception('Hash verification failed.')
-        f.truncate()
-        logging.debug('got file, extract into %s', into_directory)
-        # head back to the start of the file and untar (without closing the
-        # file)
-        f.seek(0)
-        with tarfile.open(fileobj=f) as tf:
-            to_extract = []
-            # modify members to change where they extract to!
-            for m in tf.getmembers():
-                split_path = fsutils.fullySplitPath(m.name)
-                if len(split_path) > 1:
-                    m.name = os.path.join(*(split_path[1:]))
-                    to_extract.append(m)
-            tf.extractall(path=into_directory, members=to_extract)
+                calculated_hash = m.hexdigest()
+                logging.debug('calculated hash: %s check against: %s' % (calculated_hash, hash_value))
+                if hash_value and (hash_value != calculated_hash):
+                    raise Exception('Hash verification failed.')
+            f.truncate()
+            logging.debug('got file, extract into %s (for %s)', temp_directory, into_directory)
+            # head back to the start of the file and untar (without closing the
+            # file)
+            f.seek(0)
+            with tarfile.open(fileobj=f) as tf:
+                to_extract = []
+                # modify members to change where they extract to!
+                for m in tf.getmembers():
+                    split_path = fsutils.fullySplitPath(m.name)
+                    if len(split_path) > 1:
+                        m.name = os.path.join(*(split_path[1:]))
+                        to_extract.append(m)
+                tf.extractall(path=temp_directory, members=to_extract)
+        
+        # remove the temporary download file, maybe in the future we will cache
+        # these somewhere
+        fsutils.rmRf(os.path.join(into_directory, 'download.tar.gz'))
+
+        # move the directory we extracted stuff into to where we actually want it
+        # to be
+        fsutils.rmRf(into_directory)
+        shutil.move(temp_directory, into_directory)
+
+    finally:
+        fsutils.rmRf(temp_directory)
+
     logging.debug('extraction complete %s', into_directory)
