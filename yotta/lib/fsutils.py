4aa63c537bce6fd172216d8d343ead3a289018ce
James Crosby <James.Crosby@arm.com>
2014-11-21 16:50:15 +0000

    rmRf should work on non-directories, too

--- yotta/lib/fsutils.py
+++ yotta/lib/fsutils.py
@@ -15,20 +15,22 @@ def mkDirP(path):
         if exception.errno != errno.EEXIST:
             raise
 
+def rmF(path):
+    try:
+        os.remove(path)
+    except OSError as exception: 
+        if exception.errno != errno.ENOENT:
+            raise
+
 def rmRf(path):
     try:
         shutil.rmtree(path)
     except OSError as exception:
         if 'cannot call rmtree on a symbolic link' in str(exception).lower():
             os.unlink(path)
+        elif exception.errno == errno.ENOTDIR:
+            rmF(path)
         elif exception.errno != errno.ENOENT:
-            raise
-
-def rmF(path):
-    try:
-        os.remove(path)
-    except OSError as exception: 
-        if exception.errno != errno.ENOENT:
             raise
 
 def fullySplitPath(path):
