4be02d0c500cc85670e7f47f452dd3172727ba3b
Bogdan Marinescu <bogdan.marinescu@arm.com>
2014-12-09 12:07:49 +0000

    Use 'ninja' by default on Windows
    'ninja' is much better supported (and much faster) than 'make' in
    Windows, so make it the default build method for this platform.

--- yotta/lib/target.py
+++ yotta/lib/target.py
@@ -62,7 +62,8 @@ class Target(pack.Pack):
     
     @classmethod
     def addBuildOptions(cls, parser):
-        parser.add_argument('-G', '--cmake-generator', dest='cmake_generator', default='Unix Makefiles',
+        parser.add_argument('-G', '--cmake-generator', dest='cmake_generator',
+           default=('Unix Makefiles', 'Ninja')[os.name == 'nt'],
            choices=(
                'Unix Makefiles',
                'Ninja',
