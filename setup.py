d363fae070133a97082e913916b6ffc2b245b5ad
James Crosby <James.Crosby@arm.com>
2014-12-12 11:59:13 +0000

    bump version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ platform_deps = ['ntfsutils'] if os.name == 'nt' else []
 
 setup(
     name = "yotta",
-    version = "0.0.38",
+    version = "0.0.39",
     author = "James Crosby",
     author_email = "James.Crosby@arm.com",
     description = ("Re-usable components for embedded software."),
