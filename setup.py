95c537d2b8852eb0d826c442bb054ddcbe6dc4b6
James Crosby <James.Crosby@arm.com>
2015-01-12 10:14:38 +0000

    add new jsonschema dependency to setup.py

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ setup(
         'Jinja2>=2.7.0,<3',
         pycrypto_dep,
         'PyJWT>=0.3,<0.4',
-        'pathlib>=1.0.1,<1.1'
+        'pathlib>=1.0.1,<1.1',
+        'jsonschema>=2.4.0,<3.0'
     ] + platform_deps
 )
