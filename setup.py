a7ab1ae7b62f8dea04dd578b65f6d41dd330494b
James Crosby <James.Crosby@arm.com>
2015-03-24 15:30:28 +0000

    bump version

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2014 ARM Limited
+# Copyright 2014-2015 ARM Limited
 #
 # Licensed under the Apache License, Version 2.0
 # See LICENSE file for details.
@@ -18,7 +18,7 @@ else:
 
 setup(
     name = "yotta",
-    version = "0.2.1",
+    version = "0.2.2",
     author = "James Crosby",
     author_email = "James.Crosby@arm.com",
     description = ("Re-usable components for embedded software."),
