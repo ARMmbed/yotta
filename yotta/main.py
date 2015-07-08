4e9d38d8abeeaa8bb80bb9cbf7b0e2bf67f1772a
James Crosby <James.Crosby@arm.com>
2015-07-08 15:42:10 +0100

    monkey-patch _SubParsersAction instead of subclassing - this allows argcomplete to still function on subparsers

--- yotta/main.py
+++ yotta/main.py
@@ -32,24 +32,26 @@ def splitList(l, at_value):
             r[-1].append(x)
     return r
 
+# (instead of subclassing argparse._SubParsersAction, we monkey-patch it,
+#  because argcomplete has special-cases for the _SubParsersAction class that
+#  it's impossible to extend to another class)
+#
+# add a add_parser_async function, which allows a subparser to be added whose
+# options are not evaluated until the subparser has been selected. This allows
+# us to defer loading the modules for all the subcommands until they are
+# actually:
+def _SubParsersAction_addParserAsync(self, name, *args, **kwargs):
+    if not 'callback' in kwargs:
+        raise ValueError('callback=fn(parser) argument must be specified')
+    callback = kwargs['callback']
+    del kwargs['callback']
+    parser = self.add_parser(name, *args, **kwargs)
+    parser._lazy_load_callback = callback
+    return None
+argparse._SubParsersAction.add_parser_async = _SubParsersAction_addParserAsync
 
-class LazyArgumentParser(argparse.ArgumentParser):
-    def __init__(self, *args, **kwargs):
-        super(LazyArgumentParser, self).__init__(*args, **kwargs)
-        # override the 'parsers' action to use our special lazy-loading class:
-        self.register('action', 'parsers', LazySubParsersAction)
-
-class LazySubParsersAction(argparse._SubParsersAction):
-    def add_parser_async(self, name, *args, **kwargs):
-        if not 'callback' in kwargs:
-            raise ValueError('callback=fn(parser) argument must be specified')
-        callback = kwargs['callback']
-        del kwargs['callback']
-        parser = argparse._SubParsersAction.add_parser(self, name, *args, **kwargs)
-        parser._lazy_load_callback = callback
-        return None
-
-    def __call__(self, parser, namespace, values, option_string=None):
+def _wrapSubParserActionCall(orig_call):
+    def wrapped_call(self, parser, namespace, values, option_string=None):
         parser_name = values[0]
         if parser_name in self._name_parser_map:
             subparser = self._name_parser_map[parser_name]
@@ -57,9 +59,18 @@ class LazySubParsersAction(argparse._SubParsersAction):
                 # the callback is responsible for adding the subparser's own
                 # arguments:
                 subparser._lazy_load_callback(subparser)
+                argcomplete.autocomplete(subparser)
         # now we can go ahead and call the subparser action: its arguments are
         # now all set up
-        return argparse._SubParsersAction.__call__(self, parser, namespace, values, option_string)
+        return orig_call(self, parser, namespace, values, option_string)
+    return wrapped_call
+argparse._SubParsersAction.__call__ = _wrapSubParserActionCall(argparse._SubParsersAction.__call__)
+
+
+def lazySubParserCompleter(prefix, **kwargs):
+    argcomplete.warn('lazySubParserCompleter %s %s' % (prefix, kwargs))
+    #return argcomplete.completers.ChoicesCompleter(prefix, **kwargs)
+
 
 # Override the argparse default version action so that we can avoid importing
 # pkg_resources (which is slowww) unless someone has actually asked for the
@@ -72,7 +83,7 @@ class FastVersionAction(argparse.Action):
 
 
 def main():
-    parser = LazyArgumentParser(
+    parser = argparse.ArgumentParser(
         formatter_class=argparse.RawTextHelpFormatter,
         description='Build software using re-usable components.\n'+
         'For more detailed help on each subcommand, run: yotta <subcommand> --help'
