050d269f52e4fc1c2af0b86c1cf98c6d2a77921a
Bogdan Marinescu <bogdan.marinescu@arm.com>
2015-07-20 12:33:26 +0300

    Prevent infinite loops

--- yotta/lib/fsutils_win.py
+++ yotta/lib/fsutils_win.py
@@ -28,11 +28,16 @@ def isLink(path):
 
 def tryReadLink(path):
     try:
-        prevPath = path
+        prevPath, initialPath = path, path
+        readlinkCount, maxReadlinkCount = 0, 100
         while isLink(path):
             path = junction.readlink(path)
             if path == prevPath: # no idea if this can happen, but why risk it
                 break
+            # Prevent infinite loop if circular paths are present (A->B->C->A)
+            readlinkCount = readlinkCount + 1
+            if readlinkCount == maxReadlinkCount:
+                return initialPath
             prevPath = path
         return path
     except:
