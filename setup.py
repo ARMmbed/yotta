54cccdd6c0d67006b84d9215945d84e5881325d6
James Crosby <James.Crosby@arm.com>
2015-10-23 14:20:42 +0100

    bump version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ else:
 
 setup(
     name = "yotta",
-    version = "0.8.5",
+    version = "0.9.0",
     author = "James Crosby",
     author_email = "James.Crosby@arm.com",
     description = ("Re-usable components for embedded software."),
