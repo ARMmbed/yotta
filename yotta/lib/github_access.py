65fddbf6dddf08f28e9e443eb257e6e653a8d8d4
James Crosby <James.Crosby@arm.com>
2015-11-03 15:53:32 +0000

    avoid using three-argument raise, which doesn't work in python 3

--- yotta/lib/github_access.py
+++ yotta/lib/github_access.py
@@ -47,14 +47,12 @@ def _handleAuth(fn):
         # do call auth.authorizeUser, so that a login URL can be displayed:
         interactive = globalconf.get('interactive')
 
-        def retryWithAuthOrRaise():
+        def retryWithAuthOrRaise(original_exception):
             # in all cases ask for auth, so that in non-interactive mode a
             # login URL is displayed
             auth.authorizeUser(provider='github', interactive=interactive)
             if not interactive:
-                import sys
-                exc_class, exc_obj, exc_tb = sys.exc_info()
-                raise exc_class, exc_obj, exc_tb
+                raise original_exception
             else:
                 logger.debug('trying with authtoken: %s', settings.getProperty('github', 'authtoken'))
                 return fn(*args, **kwargs)
@@ -62,29 +60,27 @@ def _handleAuth(fn):
         # authorised requests have a higher rate limit, but display a warning
         # message in this case, as the user might not expect the requirement to
         # auth:
-        def handleRateLimitExceeded():
+        def handleRateLimitExceeded(original_exception):
             if not _userAuthedWithGithub():
                 logger.warning('github rate limit for anonymous requests exceeded: you must log in')
-                return retryWithAuthOrRaise()
+                return retryWithAuthOrRaise(original_exception)
             else:
-                import sys
-                exc_class, exc_obj, exc_tb = sys.exc_info()
-                raise exc_class, exc_obj, exc_tb
+                raise original_exception
 
         try:
             return fn(*args, **kwargs)
         except requests.exceptions.HTTPError as e:
             if e.response.status_code == 403:
                 # 403 = rate limit exceeded
-                return handleRateLimitExceeded()
+                return handleRateLimitExceeded(e)
             if e.response.status_code == 401:
                 # 401 = unauthorised
-                return retryWithAuthOrRaise()
+                return retryWithAuthOrRaise(e)
             raise
-        except github.BadCredentialsException:
+        except github.BadCredentialsException as e:
             logger.debug("github: bad credentials")
-            return retryWithAuthOrRaise()
-        except github.UnknownObjectException:
+            return retryWithAuthOrRaise(e)
+        except github.UnknownObjectException as e:
             logger.debug("github: unknown object")
             # some endpoints return 404 if the user doesn't have access, maybe
             # it would be better to prompt for another username and password,
@@ -93,14 +89,14 @@ def _handleAuth(fn):
             # really is a 404
             if not _userAuthedWithGithub():
                 logger.info('failed to fetch Github object, re-trying with authentication...')
-                return retryWithAuthOrRaise()
+                return retryWithAuthOrRaise(e)
             raise
         except github.RateLimitExceededException as e:
-            return handleRateLimitExceeded()
+            return handleRateLimitExceeded(e)
         except github.GithubException as e:
             if e.status == 403:
                 # 403 = rate limit exceeded
-                return handleRateLimitExceeded()
+                return handleRateLimitExceeded(e)
             raise
     return wrapped
 
