625acadb0b3afea1ed3ec6eed7cd2c24b9ec0fbf
James Crosby <James.Crosby@arm.com>
2016-02-12 18:02:08 +0000

    ensure that yotta version spec errors in targets are reported; add test for unparseable yotta version specs; remove debug print statements

--- yotta/test/cli/test_minversion.py
+++ yotta/test/cli/test_minversion.py
@@ -34,6 +34,34 @@ Test_Min_Version_OK['module.json'] = '''{
   "yotta": ">0.0.1 !0.0.2 <999.999.999"
 }'''
 
+# NB: can't use this target to build
+Test_Min_Version_Target_Name = 'test-target'
+Test_Min_Version_Target_Insufficient = copy.copy(util.Test_Trivial_Lib)
+Test_Min_Version_Target_Insufficient['yotta_targets/test-target/target.json'] = '''{
+  "name": "%s",
+  "version": "1.0.0",
+  "license": "Apache-2.0",
+  "yotta": ">9999.9999.9999"
+}''' % Test_Min_Version_Target_Name
+
+Test_Unparsable_Spec = copy.copy(util.Test_Trivial_Lib)
+Test_Unparsable_Spec['module.json'] = '''{
+  "name": "test-trivial-lib",
+  "version": "1.0.0",
+  "description": "Module to test trivial lib compilation",
+  "license": "Apache-2.0",
+  "dependencies": {
+  },
+  "yotta": "not something that yotta can parse as a version spec"
+}'''
+Test_Unparsable_Spec['yotta_targets/test-target/target.json'] = '''{
+  "name": "%s",
+  "version": "1.0.0",
+  "license": "Apache-2.0",
+  "yotta": ">9999.9999.9999"
+}''' % Test_Min_Version_Target_Name
+
+
 class TestCLIYottaVersionSpecs(unittest.TestCase):
     @unittest.skipIf(not util.canBuildNatively(), "can't build natively on windows yet")
     def test_yottaVersionCheckTooLowBuilding(self):
@@ -47,9 +75,6 @@ class TestCLIYottaVersionSpecs(unittest.TestCase):
     def test_yottaVersionCheckOKBuilding(self):
         test_dir = util.writeTestFiles(Test_Min_Version_OK)
         stdout, stderr, statuscode = cli.run(['--target', util.nativeTarget(), 'build'], cwd=test_dir)
-        if statuscode:
-            print stderr
-            print stdout
         self.assertEqual(statuscode, 0)
         self.assertNotIn('requires yotta version >', stdout+stderr)
         util.rmRf(test_dir)
@@ -64,9 +89,22 @@ class TestCLIYottaVersionSpecs(unittest.TestCase):
     def test_yottaVersionCheckOKInstalling(self):
         test_dir = util.writeTestFiles(Test_Min_Version_OK)
         stdout, stderr, statuscode = cli.run(['--target', 'x86-linux-native', 'install'], cwd=test_dir)
-        if statuscode:
-            print stderr
-            print stdout
         self.assertEqual(statuscode, 0)
         self.assertNotIn('requires yotta version >', stdout+stderr)
         util.rmRf(test_dir)
+
+    def test_yottaVersionTargetCheck(self):
+        test_dir = util.writeTestFiles(Test_Min_Version_Target_Insufficient)
+        stdout, stderr, statuscode = cli.run(['--target', Test_Min_Version_Target_Name, 'install'], cwd=test_dir)
+        self.assertNotEqual(statuscode, 0)
+        self.assertIn('requires yotta version >', stdout+stderr)
+        util.rmRf(test_dir)
+
+    def test_unparseableSpec(self):
+        test_dir = util.writeTestFiles(Test_Unparsable_Spec)
+        stdout, stderr, statuscode = cli.run(['--target', Test_Min_Version_Target_Name, 'install'], cwd=test_dir)
+        self.assertNotEqual(statuscode, 0)
+        self.assertIn('could not parse yotta version spec', stdout+stderr)
+        util.rmRf(test_dir)
+
+
