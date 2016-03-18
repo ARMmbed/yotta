742addff68c983e2fe5c0fd2dcc83eccc684c28c
James Crosby <James.Crosby@arm.com>
2016-03-18 11:59:22 +0000

    interpret .py scripts as being relative to the root of the module/target specifying them

--- yotta/lib/pack.py
+++ yotta/lib/pack.py
@@ -510,8 +510,15 @@ class Pack(object):
                 import shlex
                 script = shlex.split(script)
             # if the command is a python script, run it with the python
-            # interpreter being used to run yotta:
+            # interpreter being used to run yotta, also fetch the absolute path
+            # to the script relative to this module (so that the script can be
+            # distributed with the module, no matter what current working
+            # directory it will be executed in):
             if len(script) and script[0].lower().endswith('.py'):
+                if not os.path.isabs(script[0]):
+                    absscript = os.path.abspath(os.path.join(self.path, script[0]))
+                    logger.debug('rewriting script %s to be absolute path %s', script[0], absscript)
+                    script[0] = absscript
                 import sys
                 script = [sys.executable] + script
 
