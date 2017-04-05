8eb8f78f10c1d16f943967491f52a25b08bb4032
Bogdan Marinescu <bogdan.marinescu@arm.com>
2017-04-05 12:07:36 +0300

    Allow sources at top level
    This commit adds the ability to use modules that have sources defined in
    their top level directory, both directly and in various subdirectories:
    
    ```
    module.json
    README.md
    src1.c
    subdir1/
      src2.c
      src3.c
      subdir2/
        src4.c
    source/
      main.c
    ```
    
    This works both for libraries and applications by specifying `"lib":
    "."` or `"bin": "."` respectively in their `module.json`.
    
    The current implementation ignores toplevel test directories (like
    `test`) so that `yotta test` can still be used with these modules.

--- yotta/lib/cmakegen.py
+++ yotta/lib/cmakegen.py
@@ -216,6 +216,10 @@ class CMakeGen(object):
         bin_subdirs = component.getBinaries()
         test_subdirs = []
         resource_subdirs = []
+        # if the application or library is set to get the sources from top level ("."),
+        # they'll be acumulated into a single array (top_sources below).
+        top_sources = []
+        start_on_top = "." in [os.path.normpath(x) for x in lib_subdirs.keys() + bin_subdirs.keys()]
         for f in sorted(os.listdir(component.path)):
             if f in Ignore_Subdirs or f.startswith('.') or f.startswith('_'):
                 continue
@@ -230,33 +234,48 @@ class CMakeGen(object):
                 if f in ('test',):
                     test_subdirs.append(f)
             else:
-                # otherwise, if the directory has source files, and is listed
-                # as a source/test directory, generate a CMakeLists in the
-                # corresponding temporary directory, and add that.
-                sources = self.containsSourceFiles(os.path.join(component.path, f), component)
-                if sources:
-                    if f in ('test',):
-                        auto_subdirs.append((f, sources))
-                        test_subdirs.append(f)
-                    elif os.path.normpath(f) in [fsutils.fullySplitPath(x)[0] for x in lib_subdirs] or \
-                         os.path.normpath(f) in [fsutils.fullySplitPath(x)[0] for x in bin_subdirs]:
-                        for full_subpath in list(lib_subdirs.keys()) + list(bin_subdirs.keys()):
-                            if fsutils.fullySplitPath(full_subpath)[0] == os.path.normpath(f):
-                                # this might be a sub-sub directory, in which
-                                # case we need to re-calculate the sources just
-                                # for the part we care about:
-                                sources = self.containsSourceFiles(os.path.join(component.path, full_subpath), component)
-                                auto_subdirs.append((full_subpath, sources))
-                    elif f == component.getName():
-                        header_subdirs.append((f, sources))
-                elif toplevel and \
-                     ((f in ('test',)) or \
-                      (os.path.normpath(f) in lib_subdirs) or \
-                      (os.path.normpath(f) in bin_subdirs) and not \
-                      component.ignores(f)):
-                    # (if there aren't any source files then do nothing)
-                    # !!! FIXME: ensure this warning is covered in tests
-                    logger.warning("subdirectory \"%s\" of %s was ignored because it doesn't appear to contain any source files", f, component)
+                if os.path.isfile(os.path.join(component.path, f)):
+                    # top level source: check if it should be included
+                    if not component.ignores(f) and start_on_top:
+                        sf = self.createSourceFile(f, os.path.join(component.path, f), ".")
+                        if sf is not None:
+                            top_sources.append(sf)
+                else:
+                    # otherwise, if the directory has source files, and is listed
+                    # as a source/test directory, generate a CMakeLists in the
+                    # corresponding temporary directory, and add that.
+                    sources = self.containsSourceFiles(os.path.join(component.path, f), component)
+                    if sources:
+                        if f in ('test',):
+                            auto_subdirs.append((f, sources))
+                            test_subdirs.append(f)
+                        elif start_on_top:
+                            # include the sources in this directory only if it's not
+                            # a potential test directory
+                            from yotta.lib import validate
+                            if not validate.isPotentialTestDir(f):
+                                top_sources.extend(sources)
+                                if f == component.getName():
+                                    header_subdirs.append((f, sources))
+                        elif os.path.normpath(f) in [fsutils.fullySplitPath(x)[0] for x in lib_subdirs] or \
+                             os.path.normpath(f) in [fsutils.fullySplitPath(x)[0] for x in bin_subdirs]:
+                            for full_subpath in list(lib_subdirs.keys()) + list(bin_subdirs.keys()):
+                                if fsutils.fullySplitPath(full_subpath)[0] == os.path.normpath(f):
+                                    # this might be a sub-sub directory, in which
+                                    # case we need to re-calculate the sources just
+                                    # for the part we care about:
+                                    sources = self.containsSourceFiles(os.path.join(component.path, full_subpath), component)
+                                    auto_subdirs.append((full_subpath, sources))
+                        elif f == component.getName():
+                            header_subdirs.append((f, sources))
+                    elif toplevel and \
+                         ((f in ('test',)) or \
+                          (os.path.normpath(f) in lib_subdirs or start_on_top) or \
+                          (os.path.normpath(f) in bin_subdirs or start_on_top) and not \
+                          component.ignores(f)):
+                        # (if there aren't any source files then do nothing)
+                        # !!! FIXME: ensure this warning is covered in tests
+                        logger.warning("subdirectory \"%s\" of %s was ignored because it doesn't appear to contain any source files", f, component)
 
             # 'resource' directory also has special meaning, but there's no
             # pattern for the files which might be in here:
@@ -270,13 +289,17 @@ class CMakeGen(object):
                f in check_directory_name_cases and not \
                component.ignores(f):
                 self.checkStandardSourceDir(f, component)
+        if top_sources:
+            # all the top level sources are grouped into a single cmake-generated directory
+            # which is given the same name as the component
+            auto_subdirs.append((component.getName(), top_sources))
 
         return {
             "manual": manual_subdirs,
               "auto": auto_subdirs,
            "headers": header_subdirs,
-               "bin": bin_subdirs,
-               "lib": lib_subdirs,
+               "bin": {component.getName(): component.getName()} if (start_on_top and component.isApplication()) else bin_subdirs,
+               "lib": {component.getName(): component.getName()} if (start_on_top and not component.isApplication()) else lib_subdirs,
               "test": test_subdirs,
           "resource": resource_subdirs
         }
@@ -759,31 +782,37 @@ class CMakeGen(object):
 
         self._writeFile(fname, file_contents)
 
-
-    def containsSourceFiles(self, directory, component):
+    def createSourceFile(self, f, fullpath, relpath):
         c_exts          = set(('.c',))
         cpp_exts        = set(('.cpp','.cc','.cxx'))
         asm_exts        = set(('.s',))
         objc_exts       = set(('.m', '.mm'))
         header_exts     = set(('.h',))
+        name, ext = os.path.splitext(f)
+        ext = ext.lower()
+        if ext in c_exts:
+            return SourceFile(fullpath, relpath, 'c')
+        elif ext in cpp_exts:
+            return SourceFile(fullpath, relpath, 'cpp')
+        elif ext in asm_exts:
+            return SourceFile(fullpath, relpath, 's')
+        elif ext in objc_exts:
+            return SourceFile(fullpath, relpath, 'objc')
+        elif ext in header_exts:
+            return SourceFile(fullpath, relpath, 'header')
+        else:
+            return None
 
+    def containsSourceFiles(self, directory, component):
         sources = []
         for root, dires, files in os.walk(directory):
             for f in sorted(files):
-                name, ext = os.path.splitext(f)
-                ext = ext.lower()
                 fullpath = os.path.join(root, f)
                 relpath  = os.path.relpath(fullpath, component.path)
                 if component.ignores(relpath):
                     continue
-                if ext in c_exts:
-                    sources.append(SourceFile(fullpath, relpath, 'c'))
-                elif ext in cpp_exts:
-                    sources.append(SourceFile(fullpath, relpath, 'cpp'))
-                elif ext in asm_exts:
-                    sources.append(SourceFile(fullpath, relpath, 's'))
-                elif ext in objc_exts:
-                    sources.append(SourceFile(fullpath, relpath, 'objc'))
-                elif ext in header_exts:
-                    sources.append(SourceFile(fullpath, relpath, 'header'))
+                sf = self.createSourceFile(f, fullpath, relpath)
+                if sf is not None:
+                    sources.append(sf)
         return sources
+
