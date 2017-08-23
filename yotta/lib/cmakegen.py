97b8d0fab8aa2d7394007d5a670100c3182d52a7
David Hyman <david.hyman@arm.com>
2017-08-23 17:14:33 +0100

    unused variables

--- yotta/lib/cmakegen.py
+++ yotta/lib/cmakegen.py
@@ -67,10 +67,6 @@ class CMakeGen(object):
         ''' Ensure all config-time files have been generated. Return a
             dictionary of generated items.
         '''
-        r = {}
-
-        builddir = self.buildroot
-
         # only dependencies which are actually valid can contribute to the
         # config data (which includes the versions of all dependencies in its
         # build info) if the dependencies aren't available we can't tell what
@@ -81,11 +77,11 @@ class CMakeGen(object):
 
         self.set_toplevel_definitions = ''
         if self.build_info_include_file is None:
-            self.build_info_include_file, build_info_definitions = self.getBuildInfo(component.path, builddir)
+            self.build_info_include_file, build_info_definitions = self.getBuildInfo(component.path, self.buildroot)
             self.set_toplevel_definitions += build_info_definitions
 
         if self.config_include_file is None:
-            self.config_include_file, config_definitions, self.config_json_file = self._getConfigData(available_dependencies, component, builddir, self.build_info_include_file)
+            self.config_include_file, config_definitions, self.config_json_file = self._getConfigData(available_dependencies, component, self.buildroot, self.build_info_include_file)
             self.set_toplevel_definitions += config_definitions
 
         self.configured = True
