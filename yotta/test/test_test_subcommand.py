0fdbef42f9e05869137e045708499cd15fdb3307
David Hyman <david.hyman@arm.com>
2017-08-29 09:34:52 +0100

    use unittest assertions properly, update for ym->modules

--- yotta/test/test_test_subcommand.py
+++ yotta/test/test_test_subcommand.py
@@ -14,11 +14,11 @@ from yotta import test_subcommand
 
 class TestTestSubcommandModule(unittest.TestCase):
     def test_moduleFromDirname(self):
-        self.assertTrue(test_subcommand.moduleFromDirname('ym/b/ym/c/d', {'b':'b', 'c':'c'}, 'a') == 'c')
-        self.assertTrue(test_subcommand.moduleFromDirname('ym/b/q/c/d', {'b':'b', 'c':'c'}, 'a') == 'b')
-        self.assertTrue(test_subcommand.moduleFromDirname('z/b/q/c/d', {'b':'b', 'c':'c'}, 'a') == 'a')
-        self.assertTrue(test_subcommand.moduleFromDirname('ym/e/d', {'b':'b', 'c':'c'}, 'a') == 'a')
-        self.assertTrue(test_subcommand.moduleFromDirname('ym/e/d', {'b':'b', 'c':'c', 'e':'e'}, 'a') == 'e')
+        self.assertEqual(test_subcommand.moduleFromDirname('modules/b/modules/c/d', {'b':'b', 'c':'c'}, 'a') , 'c')
+        self.assertEqual(test_subcommand.moduleFromDirname('modules/b/q/c/d', {'b':'b', 'c':'c'}, 'a') , 'b')
+        self.assertEqual(test_subcommand.moduleFromDirname('z/b/q/c/d', {'b':'b', 'c':'c'}, 'a') , 'a')
+        self.assertEqual(test_subcommand.moduleFromDirname('modules/e/d', {'b':'b', 'c':'c'}, 'a') , 'a')
+        self.assertEqual(test_subcommand.moduleFromDirname('modules/e/d', {'b':'b', 'c':'c', 'e':'e'}, 'a') , 'e')
 
     # see also yotta/test/cli/test.py for cli-driven testing
 
